8d056e8c2d82ffa10d9b205dab3359857bdbe00e
Timothy Dao <timster78811@gmail.com>
2018-07-25 12:02:31 -0400

    step 4 working

--- scripts/api.js
+++ scripts/api.js
@@ -5,7 +5,6 @@ const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/timothy-clint';
 
   let getItems = function(callback) {
-    callback('api module works!');
     $.getJSON(`${BASE_URL}/items`,callback);
   };
 
