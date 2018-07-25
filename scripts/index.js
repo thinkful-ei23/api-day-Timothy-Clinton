c9cc8a67d6d39e63fa4b5d5ec283d57eaff012ca
Clinton Owen <clint@clintonowen.com>
2018-07-25 10:15:36 -0500

    completed steps 1 & 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-/* global shoppingList, store */
+'use strict';
+/* global $, shoppingList, store */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
@@ -6,3 +7,9 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
+
+api.getItems(function(data) {
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
